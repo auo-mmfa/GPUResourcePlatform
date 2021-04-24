096f9180543950ba1d60b7b033b109b773b43ed8
hhji <qazwsx4256.am99@g2.nctu.edu.tw>
2021-04-25 00:06:11 +0800

    新增platform

---
+++ Platform/config.js
@@ -0,0 +1,3 @@
+var appConfig={
+    'GPU_PC_List' : '127.0.0.1,127.0.0.1,localhost,127.0.0.1,localhost,127.0.0.1,localhost,127.0.0.1'
+}
--- WebService/PythonWebService.py
+++ WebService/PythonWebService.py
@@ -3,6 +3,7 @@ import os
 import pynvml
 import subprocess
 import socket
+from flask_cors import CORS
 
 pynvml.nvmlInit()
 
@@ -21,15 +22,16 @@ def printNvidiaGPU(gpu_id):
     #UtilizationRates = pynvml.nvmlDeviceGetUtilizationRates(gpu_device)
     print("GPU Id: "+ str(i) )
     print("GPU Name: "+ gpu_name.decode("utf-8") )
-    print("MemoryInfo°G{0}M/{1}M°A®œ•Œ≤v°G{2}%".format("%.1f" % (usedMemory / 1024 / 1024), "%.1f" % (totalMemory / 1024 / 1024), "%.1f" % (usedMemory/totalMemory*100)))
-    print("Temperature°G{0}¢XC".format(temperature))
-    #print("Performance°G{0}".format(performance))
+    print("MemoryInfoÔºö{0}M/{1}MÔºå‰ΩøÁî®ÁéáÔºö{2}%".format("%.1f" % (usedMemory / 1024 / 1024), "%.1f" % (totalMemory / 1024 / 1024), "%.1f" % (usedMemory/totalMemory*100)))
+    print("TemperatureÔºö{0}¬∞C".format(temperature))
+    #print("PerformanceÔºö{0}".format(performance))
     #print("PowerState: {0}".format(powerState))
     #print("PowerUsage: {0}".format(powerUsage / 1000))
     #print("FanSpeed: {0}".format(FanSpeed))
     #print("UtilizationRates: {0}".format(UtilizationRates.gpu))
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/",methods=["GET"])
 def welcome():
@@ -43,10 +45,15 @@ def GetActiveUsers():
     line = results[0].split("\n")
     activeUsers = []
     for i in range(len(line)):
-        if("®œ•Œ§§" in line[i]):
+        if("‰ΩøÁî®‰∏≠" in line[i]):
             userInfo = line[i].split("  ")[0].strip()
             activeUsers.append(userInfo)
-    return "µn§J§§™∫User: "+str(activeUsers)
+    return "ÁôªÂÖ•‰∏≠ÁöÑUser: "+str(activeUsers)
+
+@app.route("/GetHostname",methods=["GET"])
+def GetHostname():
+    hostname = socket.gethostname()
+    return "ÈõªËÖ¶ÂêçÁ®±: "+str(hostname)    
 
 @app.route("/GetNvidiaGPUInfo",methods=["GET"])
 def GetNvidiaGPUInfo():
@@ -65,22 +72,29 @@ def GetNvidiaGPUInfo():
         #FanSpeed = pynvml.nvmlDeviceGetFanSpeed(gpu_device)
         #UtilizationRates = pynvml.nvmlDeviceGetUtilizationRates(gpu_device)
         
-        result += "GPU Id: "+ str(gpu_id) + "\n"
+        result += "GPU Id: "+ str(gpu_id) + "<br>"
         #print("GPU Id: "+ str(gpu_id))
-        result += "GPU Name: "+ gpu_name.decode("utf-8") + "\n"
+        result += "GPU Name: "+ gpu_name.decode("utf-8") + "<br>"
         #print("GPU Name: "+ gpu_name.decode("utf-8"))
-        result += "MemoryInfo°G{0}M/{1}M°A®œ•Œ≤v°G{2}%".format("%.1f" % (usedMemory / 1024 / 1024), "%.1f" % (totalMemory / 1024 / 1024), "%.1f" % (usedMemory/totalMemory*100)) + "\n"
-        #print("MemoryInfo°G{0}M/{1}M°A®œ•Œ≤v°G{2}%".format("%.1f" % (usedMemory / 1024 / 1024), "%.1f" % (totalMemory / 1024 / 1024), "%.1f" % (usedMemory/totalMemory*100)))
-        result += "Temperature°G{0}¢XC".format(temperature) + "\n"
-        #print("Temperature°G{0}¢XC".format(temperature))
+        result += "MemoryInfoÔºö{0}M/{1}MÔºå‰ΩøÁî®ÁéáÔºö{2}%".format("%.1f" % (usedMemory / 1024 / 1024), "%.1f" % (totalMemory / 1024 / 1024), "%.1f" % (usedMemory/totalMemory*100)) + "<br>"
+        #print("MemoryInfoÔºö{0}M/{1}MÔºå‰ΩøÁî®ÁéáÔºö{2}%".format("%.1f" % (usedMemory / 1024 / 1024), "%.1f" % (totalMemory / 1024 / 1024), "%.1f" % (usedMemory/totalMemory*100)))
+        result += "TemperatureÔºö{0}¬∞C".format(temperature) + "<br>"
+        #print("TemperatureÔºö{0}¬∞C".format(temperature))
         
-        #print("Performance°G{0}".format(performance))
+        #print("PerformanceÔºö{0}".format(performance))
         #print("PowerState: {0}".format(powerState))
         #print("PowerUsage: {0}".format(powerUsage / 1000))
         #print("FanSpeed: {0}".format(FanSpeed))
         #print("UtilizationRates: {0}".format(UtilizationRates.gpu))  
 
     return result
+
+@app.route("/GetResourceInfo",methods=["GET"])
+def GetResourceInfo(): 
+    hostname = socket.gethostname()
+    ResourceInfo = ""
+    ResourceInfo += GetHostname() +"<hr size=""2"" align=""center"" noshade width=""100%"" color=""#D3D3D3"">"+ GetActiveUsers() + "<br>" + GetNvidiaGPUInfo() 
+    return ResourceInfo
 
 if(__name__=="__main__") :
     app.run(host='0.0.0.0', port=80,threaded=True,debug=False)

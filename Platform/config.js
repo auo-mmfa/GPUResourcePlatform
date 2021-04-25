f58b4e913b673b3586bce2f7aff6fed16731efc8
hhji <qazwsx4256.am99@g2.nctu.edu.tw>
2021-04-25 14:18:23 +0800

    修改config.js格式

--- Platform/config.js
+++ Platform/config.js
@@ -1,3 +1,12 @@
 var appConfig={
-    'GPU_PC_List' : '127.0.0.1,127.0.0.1,localhost,127.0.0.1,localhost,127.0.0.1,localhost,127.0.0.1'
+    'GPU_PC_List' :  '\
+								127.0.0.1 \
+								127.0.0.1 \
+								localhost \
+								127.0.0.1 \
+								localhost \
+								127.0.0.1 \
+								localhost \
+								127.0.0.1 \
+								'
 }
